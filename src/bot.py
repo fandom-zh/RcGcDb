dafc849321e5d196d4c8dd8c83a989558dc20009
Frisk <furrtrex43@protonmail.ch>
2020-07-10 01:58:25 +0200

    whatever, i'm just testing

--- src/bot.py
+++ src/bot.py
@@ -1,12 +1,20 @@
 import logging.config
 from src.config import settings
+import sqlite3
+from src.wiki import Wiki
 
 logging.config.dictConfig(settings["logging"])
 logger = logging.getLogger("rcgcdb.bot")
 logger.debug("Current settings: {settings}".format(settings=settings))
 
-# Fetch basic information about all of the wikis in the database
+conn = sqlite3.connect('rcgcdb.db')
+c = conn.cursor()
 
+# Fetch basic information about all of the wikis in the database
+all_wikis = {}
+
+for wiki in c.execute('SELECT * FROM wikis'):
+	all_wikis[wiki[0]] = Wiki()  # assign cached information
 
 
 # Start queueing logic
--- src/wiki.py
+++ src/wiki.py
@@ -1,1 +1,5 @@
+from dataclasses import dataclass
+
+@dataclass
 class Wiki:
+	fail_times: int = 0  # corresponding to amount of times connection with wiki failed for client reasons (400-499)
