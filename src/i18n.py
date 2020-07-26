7cdafccb7854cf49b3629122bd9b916ed8e384ab
MarkusRost <sukramxro@gmail.com>
2020-07-26 14:17:57 +0000

    Update i18n.py

--- src/i18n.py
+++ src/i18n.py
@@ -6,11 +6,12 @@ try:
 	en = gettext.translation('rcgcdw', localedir='locale', languages=["en"])
 	de = gettext.translation('rcgcdw', localedir='locale', languages=["de"])
 	pl = gettext.translation('rcgcdw', localedir='locale', languages=["pl"])
-	pt = gettext.translation('rcgcdw', localedir='locale', languages=["pt-br"])
-	ru = gettext.translation('rcgcdw', localedir='locale', languages=["ru"])
-	uk = gettext.translation('rcgcdw', localedir='locale', languages=["uk"])
-	fr = gettext.translation('rcgcdw', localedir='locale', languages=["fr"])
-	langs = {"en": en, "de": de, "pl": pl, "pt": pt, "ru": ru, "uk": uk, "fr": fr}
+	#pt = gettext.translation('rcgcdw', localedir='locale', languages=["pt-br"])
+	#ru = gettext.translation('rcgcdw', localedir='locale', languages=["ru"])
+	#uk = gettext.translation('rcgcdw', localedir='locale', languages=["uk"])
+	#fr = gettext.translation('rcgcdw', localedir='locale', languages=["fr"])
+	langs = {"en": en, "de": de, "pl": pl}
+	#langs = {"en": en, "de": de, "pl": pl, "pt": pt, "ru": ru, "uk": uk, "fr": fr}
 except FileNotFoundError:
 	logger.critical("No language files have been found. Make sure locale folder is located in the directory.")
 	sys.exit(1)
