0e46d4c971a3d35ea6a2b82744eb369e4c2a29c3
Frisk <furrtrex43@protonmail.ch>
2020-08-02 20:59:17 +0200

    Initial work

--- src/wiki_ratelimiter.py
+++ src/wiki_ratelimiter.py
@@ -1,7 +1,10 @@
 import logging
+from urllib.parse import urlparse
 
 logger = logging.getLogger("rcgcdw.ratelimiter")
 
 class RateLimiter:
 	def __init__(self):
 		self.domain_requests: dict = {}
+
+	def get_timeout(self, url):
