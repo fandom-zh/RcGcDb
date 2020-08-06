cdd90de8a5bdedc4588e9eabc11f3d9acdd88ef7
Markus-Rost <sukramxro@gmail.com>
2020-08-06 16:54:15 +0200

    pt to ptbr

--- src/i18n.py
+++ src/i18n.py
@@ -6,12 +6,12 @@ try:
 	en = gettext.translation('rcgcdw', localedir='locale', languages=["en"])
 	de = gettext.translation('rcgcdw', localedir='locale', languages=["de"])
 	pl = gettext.translation('rcgcdw', localedir='locale', languages=["pl"])
-	pt = gettext.translation('rcgcdw', localedir='locale', languages=["pt-br"])
+	ptbr = gettext.translation('rcgcdw', localedir='locale', languages=["pt-br"])
 	#ru = gettext.translation('rcgcdw', localedir='locale', languages=["ru"])
 	#uk = gettext.translation('rcgcdw', localedir='locale', languages=["uk"])
 	#fr = gettext.translation('rcgcdw', localedir='locale', languages=["fr"])
-	langs = {"en": en, "de": de, "pl": pl, "pt": pt}
-	#langs = {"en": en, "de": de, "pl": pl, "pt": pt, "ru": ru, "uk": uk, "fr": fr}
+	langs = {"en": en, "de": de, "pl": pl, "pt-br": ptbr}
+	#langs = {"en": en, "de": de, "pl": pl, "pt-br": ptbr, "ru": ru, "uk": uk, "fr": fr}
 except FileNotFoundError:
 	logger.critical("No language files have been found. Make sure locale folder is located in the directory.")
 	sys.exit(1)
