65c5c54c3f113b52354290441838ce5df03a02d2
Frisk <furrtrex43@protonmail.ch>
2020-08-15 10:59:51 +0200

    Delete unused module

--- src/session.py
+++
@@ -1,8 +0,0 @@
-import aiohttp
-from src.config import settings
-session = None
-
-
-async def start_session():
-	global session
-	session = aiohttp.ClientSession(headers=settings["header"], timeout=aiohttp.ClientTimeout(5.0))
